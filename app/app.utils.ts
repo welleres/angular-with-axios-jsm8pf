5c2a533d68d762c2c071a8a579ff9151ac6f6884
welleres <34740986+welleres@users.noreply.github.com>
2023-03-31 11:32:48 +0200

    1 file modified, 1 added

---
+++ app/app.utils.ts
@@ -0,0 +1,12 @@
+import { isDevMode } from '@angular/core';
+const urls = {
+  DEV: 'https://randomuser.me',
+  PROD: 'https://randomuser.me',
+};
+export default function getUrl(path: string) {
+  if (isDevMode()) {
+    return urls.DEV + path;
+  }
+
+  return urls.PROD + path;
+}
--- app/repository/user.ts
+++ app/repository/user.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
+import getUrl from '../app.utils';
 import httpClient from '../infrastructure/http-client';
 
 @Injectable()
 export class UserRepository {
   async random() {
-    const resp = await fetch('https://randomuser.me/api/');
+    const url = getUrl('/api');
+    const resp = await fetch(url);
     const data = await resp.json();
     const {
       results: [user],
